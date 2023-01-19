b1852d86ecfbfec247020b214266068340417280
Danilo <bdortiz05@ucatolica.edu.co>
2023-01-18 20:58:42 -0500

    Segunda version

---
+++ CursoPython/00_Hello.py
@@ -0,0 +1,37 @@
+"""
+Primeras dos horas de curso de   https://www.youtube.com/watch?v=Kp4Mvapo5kc&t=1333s
+
+@ MoureDev by Brais Moure
+
+
+"""
+
+print("prueba hola mundo")
+
+Texto = "Hola mundo"
+
+print  (type(Texto))
+
+# Operations
+
+print(2 + 3)   # addition(+)
+print(3 - 1)   # subtraction(-)
+print(2 * 3)   # multiplication(*)
+print(3 / 2)   # division(/)
+print(3 ** 2)  # exponential(**)
+print(3 % 2)   # modulus(%)
+print(3 // 2)  # Floor division operator(//)
+
+# Checking data types
+
+print(type(10))                  # Int
+print(type(3.14))                # Float
+print(type(1 + 3j))              # Complex
+print(type('Asabeneh'))          # String
+print(type([1, 2, 3]))           # List
+print(type({'name':'Asabeneh'})) # Dictionary
+print(type({9.8, 3.14, 2.7}))    # Set
+print(type((9.8, 3.14, 2.7)))    # Tuple
+
+
+

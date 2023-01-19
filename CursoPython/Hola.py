37321b3d1a33136b9a33c1cd3532fc877860a59d
Danilo <bdortiz05@ucatolica.edu.co>
2023-01-18 20:19:42 -0500

    Actualizando los archivos del curso py 1ra

---
+++ CursoPython/Hola.py
@@ -0,0 +1,15 @@
+"""
+Primeras dos horas de curso de   https://www.youtube.com/watch?v=Kp4Mvapo5kc&t=1333s
+
+@ MoureDev by Brais Moure
+
+
+"""
+
+print("prueba hola mundo")
+
+Texto = "Hola mundo"
+
+print  (type(Texto))
+
+

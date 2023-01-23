01b0b73266fa6b4e873e21a673ca8327133dca73
Danilo <bdortiz05@ucatolica.edu.co>
2023-01-23 15:00:41 -0500

    Trabajo del dia 23/01/2023

--- CursoPython/02_operator.py
+++ CursoPython/02_operator.py
@@ -5,8 +5,8 @@ print(3 - 1)   # subtraction(-)
 print(2 * 3)   # multiplication(*)
 print(3 / 2)   # division(/)
 print(3 ** 2)  # exponential(**)
-print(3 % 2)   # modulus(%)
-print(3 // 2)  # Floor division operator(//)
+print(3 % 2)   # modulus(%) Modulo de la division  
+print(3 // 2)  # Floor division operator(//) Division a numero entero
 """
 
 print(3 + 4)

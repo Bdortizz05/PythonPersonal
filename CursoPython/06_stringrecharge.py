799e9a8f25d8f31b5ba38f9d4d843a9df7aba193
Danilo <bdortiz05@ucatolica.edu.co>
2023-01-25 19:10:45 -0500

    Fin trabajo el dia de hoy

---
+++ CursoPython/06_stringrecharge.py
@@ -0,0 +1,15 @@
+#
+
+
+text  = 'she know programming in Python'
+print('Python' in text)
+
+print('python' in text)
+
+
+if 'prueba' in text:
+  print('thats correct of word-> Python')
+elif 'python' in text:
+  print('thats correct of word-> python')
+else:
+  print('its no correct word')
---
+++ CursoPython/Proyecto.py
@@ -0,0 +1,11 @@
+#
+player1 = (input('Ingrese "pieda,papel o tijera" : '))
+cpu = (input('Ingrese "pieda,papel o tijera" : '))
+print(f"player= {player1}  cpu={cpu}")
+
+if cpu == "papel" and player1 == "tijera" or cpu == "piedra" and player1 == "papel" or cpu == "tijera" and player1 == "piedra":
+    print("Win player")
+elif cpu == "tijera" and player1 == "papel" or cpu == "piedra" and player1 == "tijera" or cpu == "papel" and player1 == "piedra":
+    print("Win cpu")
+else:
+    print("empate")

4ed78820f86c413e48825be5f4fa70bd5de76a61
Danilo <bdortiz05@ucatolica.edu.co>
2023-01-30 20:27:27 -0500

    Inicio de clase 30/01/20223

--- CursoPython/06_stringrecharge.py
+++ CursoPython/06_stringrecharge.py
@@ -13,3 +13,28 @@ elif 'python' in text:
   print('thats correct of word-> python')
 else:
   print('its no correct word')
+
+"""
+Comandos para strings
+
+Variable.upper()-> ponerlos  en mayuscuula
+Variable.lower() ->  ponerlos en minuscula
+len ->  peso  de el string 
+Variable.count('a') ->  validar las veces que esta la letra 
+"""
+
+print('fin')
+print(text.upper())
+print(text.lower())
+print(text.count('a'))
+print(text.swapcase())
+print(text.startswith('he'))
+print(text.replace('she', 'he'))
+
+text_2 =  'este es un titulo'
+
+print(text_2.capitalize())
+
+print(text_2.title())
+
+print(text_2.islower())

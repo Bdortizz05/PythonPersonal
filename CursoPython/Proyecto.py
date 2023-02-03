a878e010ef4b152bf06b80fe4ae67f3ed02ab866
Danilo <bdortiz05@ucatolica.edu.co>
2023-02-02 23:03:06 -0500

    Pruebas del programa de proyecto

--- CursoPython/Proyecto.py
+++ CursoPython/Proyecto.py
@@ -1,13 +1,24 @@
 #
-player1 = (input('Ingrese "pieda,papel o tijera" : '))
-cpu = (input('Ingrese "pieda,papel o tijera" : '))
-player1 = player1.lower
-cpu = cpu.lower
-print(f"player= {player1}  cpu={cpu}")
 
-if cpu == "papel" and player1 == "tijera" or cpu == "piedra" and player1 == "papel" or cpu == "tijera" and player1 == "piedra":
-    print("Win player")
-elif cpu == "tijera" and player1 == "papel" or cpu == "piedra" and player1 == "tijera" or cpu == "papel" and player1 == "piedra":
-    print("Win cpu")
-else:
-    print("empate")
+COUNT_CPU = 0
+COUNT_USER = 0
+
+while True:
+    player1 = (input('Ingrese "piedra, papel o tijera" player: '))
+    cpu = (input('Ingrese "piedra, papel o tijera" cpu: '))
+
+    if cpu == "papel" and player1 == "tijera" or cpu == "piedra" and player1 == "papel" or cpu == "tijera" and player1 == "piedra":
+        COUNT_USER += 1
+        print("Win player ->", COUNT_USER)
+    elif cpu == "tijera" and player1 == "papel" or cpu == "piedra" and player1 == "tijera" or cpu == "papel" and player1 == "piedra":
+        COUNT_CPU += 1
+        print("Win cpu ->", COUNT_CPU)
+    else:
+        print("empate")
+
+    if COUNT_CPU == 3 or COUNT_USER == 3:
+        if COUNT_CPU == 3:
+            print('Gana CPU')
+        else:
+            print('Gana player')
+        break
--- CursoPython/prueba.py
+++ CursoPython/prueba.py
@@ -1,9 +1,49 @@
+import random
 
-my_list = [1, -1, 2, -2, 3, -3, 4, -4]
-new_list = []
+option = ['Piedra', 'Papel', 'Tijera']
+computer_selection = random.choice(option)
+user_count = 0
+computer_count = 0
+round_count = 1
+computer_win = 'El computador gana la ronda'
+user_win = 'El jugador gana la ronda'
 
-# Escribe tu solución 👇
-for element in  my_list:
-    if element > 0:
-        new_list.append(element)
-print(new_list)
+print("La 3ra es la vencida")
+
+while round_count < 4:
+  print(f'Ronda {round_count}')
+  user_selection = (input('Piedra, Papel o Tijera: ')).title()
+  if user_selection in option:
+      print(f'La computadora seleccionó {computer_selection}')
+      print(f'El jugador seleccionó {user_selection}')
+  else:
+      print('Selecciona una opción valida')
+      continue
+  user_selection_position = option.index(user_selection)
+  computer_selection_position = option.index(computer_selection)
+  if user_selection_position == computer_selection_position:
+    print(f'Empate en la ronda {round_count}')
+  elif user_selection_position == 2 and computer_selection_position == 0:
+    print(f'{computer_selection} gana a {user_selection}')
+    print(f'{computer_win} {round_count}')
+    computer_count += 1
+  elif user_selection_position == 0 and computer_selection_position == 2:
+    print(f'{user_selection} gana a {computer_selection}')
+    print(f'{user_win} {round_count}')
+    user_count += 1
+  elif user_selection_position < computer_selection_position:
+    print(f'{computer_selection} gana a {user_selection}')
+    print(f'{computer_win} {round_count}')
+    computer_count += 1
+  else:
+    print(f'{user_selection} gana a {computer_selection}')
+    print(f'{user_win} {round_count}')
+    user_count += 1
+  round_count += 1
+  print(f'El jugador lleva {user_count} puntos y el computador {computer_count} puntos')
+if user_count == computer_count:
+  print("El juego termina en empate")
+elif user_count < computer_count:
+  print("El computador gana el juego")
+else:
+  print("El jugador gana el juego")

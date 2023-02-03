3fe06a0a742faa648a0d82f677fa732bfaf6d880
Danilo <bdortiz05@ucatolica.edu.co>
2023-02-03 16:39:33 -0500

    Primeros pasos

--- Introducción al Pensamiento Computacional con Python/01_Prueba.py
+++ Introducción al Pensamiento Computacional con Python/01_Prueba.py
@@ -1,1 +1,10 @@
-print('hola')
+my_str = 'pLaTzi'
+len(my_str)
+
+print('my_str = ' + my_str)
+print('len(my_str) = ' + str(len(my_str)))
+print(my_str[0:5])
+print(my_str[5])
+print(my_str.capitalize())
+print(my_str.casefold())
+print(len(my_str.center(20)),my_str.center(20))

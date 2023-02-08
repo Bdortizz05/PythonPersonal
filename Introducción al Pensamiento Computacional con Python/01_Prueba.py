0957dd65c91de8a78f5097145ed893d71ce37ca0
Danilo <bdortiz05@ucatolica.edu.co>
2023-02-08 15:51:52 -0500

    retomando

--- Introducción al Pensamiento Computacional con Python/01_Prueba.py
+++ Introducción al Pensamiento Computacional con Python/01_Prueba.py
@@ -8,3 +8,18 @@ print(my_str[5])
 print(my_str.capitalize())
 print(my_str.casefold())
 print(len(my_str.center(20)),my_str.center(20))
+
+contador_externo = 0
+contador_interno = 0
+
+while contador_externo < 11:
+    while contador_interno < 11:
+        print(contador_externo, contador_interno)
+        contador_interno += 1
+
+        if contador_interno >= 10:
+            break
+
+    contador_externo += 1
+    contador_interno = 0
+
